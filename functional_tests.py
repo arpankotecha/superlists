5b1df0abc33b41815f764a6b976b7ebd42d0cfd6
Arpan Kotecha <arpankotecha@Arpans-MacBook-Pro.local>
2016-11-15 14:27:09 -0500

    Refactor homepage view to use a template

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -17,6 +18,33 @@ class NewVisitorTest(unittest.TestCase):
 
     # Notice page title and header mention to-do lists
     self.assertIn('To-Do', self.browser.title)
+    header_text = self.browser.find_element_by_tag_name(
+            'h1').text
+    self.assertIn('To-Do', header_text)
+
+    # Invited to enter a to-do item right away
+    inputbox = self.browser.find_element_by_id(
+      'id_new_item')
+    self.assertEqual(
+      inputbox.get_attribute('placeholder'),
+      'Enter a to-do item')
+
+    # The user enters "Buy Peacock Feathers" into a text
+    # box
+    inputbox.send_keys('Buy Peacock Feathers')
+
+    # When the user presses enter, the page updates and now
+    # the page lists 1: Buy Peacock Feathers as an item in
+    # the to-do list table
+    inputbox.send_keys(Keys.ENTER)
+
+    table = self.browser.find_element_by_id(
+        'id_list_table')
+    rows = table.find_elements_by_tag_name('tr')
+    self.assertTrue(
+        any(row.text == '1: Buy Peacock Feathers' for row \
+            in rows))
+
     self.fail("Finish this test")
   # end
 # end
