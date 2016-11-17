8b77a6f597b5bb4d34a42a2ec51dc6fa67be4ebc
Arpan Kotecha <arpankotecha@Arpans-MacBook-Pro.local>
2016-11-17 08:33:00 -0500

    Add concept of lists

--- lists/tests.py
+++ lists/tests.py
@@ -5,7 +5,7 @@ from django.core.urlresolvers import resolve
 from django.template.loader import render_to_string
 
 from lists.views import home_page
-from lists.models import Item
+from lists.models import Item, List
 
 class HomePageTest(TestCase):
   def test_root_url_resolves_to_home_page_view(self):
@@ -22,15 +22,23 @@ class HomePageTest(TestCase):
   # end
 # end
 
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
   def test_saving_and_retrieving(self):
+    list_ = List()
+    list_.save()
+
     first_item = Item()
     first_item.text = "The first (ever) list item"
+    first_item.list = list_
     first_item.save()
 
     second_item = Item()
     second_item.text = "Item the second"
+    second_item.list = list_
     second_item.save()
+
+    saved_list = List.objects.first()
+    self.assertEqual(saved_list, list_)
 
     saved_items = Item.objects.all()
     self.assertEqual(saved_items.count(), 2)
@@ -39,8 +47,12 @@ class ItemModelTest(TestCase):
     second_saved_item = saved_items[1]
     self.assertEqual(first_saved_item.text, 
         "The first (ever) list item")
+    self.assertEqual(first_saved_item.list,
+        list_)
     self.assertEqual(second_saved_item.text,
         "Item the second")
+    self.assertEqual(second_saved_item.list,
+        list_)
   # end
 # end
 
@@ -52,8 +64,9 @@ class ListViewTest(TestCase):
   # end
 
   def test_displays_all_items(self):
-    Item.objects.create(text="itemey 1")
-    Item.objects.create(text="itemey 2")
+    list_ = List.objects.create()
+    Item.objects.create(text="itemey 1", list=list_)
+    Item.objects.create(text="itemey 2", list=list_)
 
     response = self.client.get(
         '/lists/the-only-list-in-the-world')
