65dbf7279cb353815cf42d51ea50766c1e823629
Arpan Kotecha <arpankotecha@Arpans-MacBook-Pro.local>
2016-11-18 13:02:15 -0500

    Allow FT to be able to run on staging website

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -3,8 +3,25 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 import unittest
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+import sys
 
 class NewVisitorTest(StaticLiveServerTestCase):
+  @classmethod
+  def setUpClass(cls):
+    for arg in sys.argv:
+      if 'liveserver' in arg:
+        cls.server_url = "http://" + arg.split('=')[1]
+        return 
+    super().setUpClass()
+    cls.server_url = cls.live_server_url
+  #end
+
+  @classmethod
+  def tearDownClass(cls):
+    if cls.server_url == cls.live_server_url:
+      super().tearDownClass()
+  #end
+
   def setUp(self):
     self.browser = webdriver.Chrome()
     self.browser.implicitly_wait(5)
@@ -22,7 +39,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
   def test_can_start_a_list_and_retrieve_it_later(self):
     # User goes online to check out the home page
-    self.browser.get(self.live_server_url)
+    self.browser.get(self.server_url)
 
     # Notice page title and header mention to-do lists
     self.assertIn('To-Do', self.browser.title)
@@ -69,7 +86,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     # Francis visits the home page. There is no sign of 
     # Ediths list
-    self.browser.get(self.live_server_url)
+    self.browser.get(self.server_url)
     page_text = self.browser.find_element_by_tag_name(
         'body').text
     self.assertNotIn('Buy Peacock Feathers', page_text)
@@ -95,7 +112,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
   
   def test_layout_and_styling(self):
     # Edith goes to the homepage
-    self.browser.get(self.live_server_url)
+    self.browser.get(self.server_url)
     self.browser.set_window_size(1024, 768)
 
     # She notices that the input box is nicely centered
